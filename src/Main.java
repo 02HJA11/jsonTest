08a14670b9cedd74f1b4ba82de5ac2f89d068035
Johan <hansjohanaxmon@gmail.com>
2023-03-09 10:57:46 +0100

    added json and fixed java and all that jazz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("Hello world!");
 
         JSONObject JSONob = new JSONObject();
@@ -11,5 +17,14 @@ public class Main {
 
         System.out.println("Mitt namn är " + JSONob.get("name"));
         System.out.println("Jag är " + JSONob.get("age"));
+
+
+        Object o = new JSONParser().parse(new FileReader("C:\\Users\\Lego_\\IdeaProjects\\jsonTest\\src\\data.json"));
+        JSONObject jsonData = (JSONObject) o;
+
+        JSONObject person = (JSONObject) jsonData.get("p1");
+        String name = (String) person.get("name");
+
+        System.out.println("P1 Name: " + name);
     }
 }
